6f244a4205731a571acf78141a1607e35bed50e5
Shwetha V <slnu2@luc.edu>
2024-09-24 18:46:51 -0500

    codes

---
+++ meeting.js
@@ -0,0 +1,14 @@
+//Call
+cwebTestFunction();
+
+//Define
+function cwebTestFunction(){
+
+   var name = "S.Vaidyanathan";
+   var subject = "Web Tooling Class";
+   var meetingTime;
+
+   meetingTime = "6pm";
+
+   //String Interpolation
+   console.log(`Name:${name} Subject: ${subject} Meeting Time: ${meetingTime}`)}
